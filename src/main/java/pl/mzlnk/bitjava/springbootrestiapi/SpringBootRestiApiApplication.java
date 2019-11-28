49331dfb243eb4e51a9ca49d1b67d023d32787cf
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2019-11-28 19:17:14 +0100

    init - added sources for empty project

---
+++ src/main/java/pl/mzlnk/bitjava/springbootrestiapi/SpringBootRestiApiApplication.java
@@ -0,0 +1,15 @@
+package pl.mzlnk.bitjava.springbootrestiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class SpringBootRestiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestiApiApplication.class, args);
+    }
+
+}
